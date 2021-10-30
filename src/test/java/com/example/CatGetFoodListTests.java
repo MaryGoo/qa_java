33385df5efa1cf647abf3c0982022d873844b91a
MaryGoo <m381988@gmail.com>
2021-10-30 16:49:38 +0300

    CatGetSoundTests

--- src/test/java/com/example/CatGetFoodListTests.java
+++ src/test/java/com/example/CatGetFoodListTests.java
@@ -2,26 +2,26 @@ package com.example;
 
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class CatGetFoodListTests {
 
-    Feline feline;
+  //  Feline feline;
     List <String> foods = List.of("Сухой корм", "Птицы", "Рыба");
 
     @Mock
-    Predator predator;
+    private Predator predator;
+    @Mock
+    private Feline feline;
 
     @Test
     public void getFoodListTwoValuesSuccess() throws Exception{
+        when(predator.eatMeat()).thenReturn(foods);
         Cat cat = new Cat (feline);
-       // Mockito.when(feline.eatMeat()).thenReturn(foods);
-        assertEquals(cat.getFood(), foods);
-        Mockito.verify(cat, Mockito.times(1)).predator.eatMeat();
+       // assertEquals(cat.getFood(), foods);
+        verify(predator).eatMeat();
     }
 }
